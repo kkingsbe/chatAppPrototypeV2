d7a94f0cfaafee23ffb90deb4d2ea392f0076714
Kyle <kkingsbe@gmail.com>
2018-06-25 20:20:59 -0400

    Initial commit

---
+++ src/sample/loginController.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class loginController {
+    public Button loginBtn;
+    public TextField username;
+
+    public void login(ActionEvent actionEvent) {
+    }
+}
